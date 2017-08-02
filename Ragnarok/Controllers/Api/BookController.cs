92f03d03ed333e65cb3136e9d30c8396dda3afb7
DiaPasc <dia.pasc@gmail.com>
2017-08-02 15:51:58 +0300

    list book api

---
+++ Ragnarok/Controllers/Api/BookController.cs
@@ -0,0 +1,35 @@
+﻿using Ragnarok.DA;
+using Ragnarok.DA.Repositories;
+using Ragnarok.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace Ragnarok.Controllers.Api
+{
+    public class BookController : ApiController
+    {
+        private BookDBContext dbContext;
+        private UnitOfWork unitOfWork;
+
+        public BookController()
+        {
+            dbContext = new BookDBContext();
+            unitOfWork = new UnitOfWork(dbContext);
+        }
+
+        [HttpGet]
+        public Book[] GetData()
+        {
+            Book[] books = unitOfWork.BookRepository.List().Select(x => new Book
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Author = x.Author,
+                Year = x.Year,
+                Description = x.Description
+            }).ToArray();
+
+            return books;
+        }
+    }
+}
