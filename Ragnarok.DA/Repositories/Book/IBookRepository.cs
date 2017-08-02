9e1c57ee876071255e653e1b5613c75876da9e60
DiaPasc <dia.pasc@gmail.com>
2017-08-02 09:31:46 +0300

    Book Repository Class

---
+++ Ragnarok.DA/Repositories/Book/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ragnarok.DA.Repositories
+{
+    public interface IBookRepository : IBaseRepository<Book.Entity.Book>
+    {
+        Book.Entity.Book[] List();
+        void Create(Book.Entity.Book book);
+        void Update(Book.Entity.Book book);
+        void Save();
+    }
+}
